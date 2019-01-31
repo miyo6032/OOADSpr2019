79cb4f4e1ecf40684e3d444c42a5e4288aff9ba3
miyo6032 <miyo6032@colorado.edu>
2019-01-30 17:18:59 -0700

    Make the base shape class abstract

--- HomeworkOne.py
+++ HomeworkOne.py
@@ -2,17 +2,24 @@
 #OODA 4448 Homework One, Problem IV
 #Bruce Montgomery, Spring 2019
 
+# If this is not found, run 'pip install Lib' to install the abc module
+from abc import ABC, abstractmethod
 
-#abstract class 
-class shapeNames():  
+# The shapeNames class is an abstract class that represents a general shape.
+# Inhereting from ABC makes it abstract
+class shapeNames(ABC):
 
+	# Returns the number of edges for a shape
+	@abstractmethod
 	def getEdges (self): 
 		pass
 
+	# Displays the name for a shape
+	@abstractmethod
 	def displayFunction(self):  
 		pass 
 
-#class for name and edges: Square
+# class for name and edges: Square
 class square(shapeNames): 
 	def getEdges(self):  
 		return 4  
@@ -20,7 +27,7 @@ class square(shapeNames):
 	def displayFunction(self): 
 		print("square")
 
-#class for name and edges: Circle 
+# class for name and edges: Circle 
 class circle(shapeNames): 
 	def getEdges(self):  
 		return 0  
@@ -28,7 +35,7 @@ class circle(shapeNames):
 	def displayFunction(self): 
 		print("circle")
 
-#class for name and edges: Triangle
+# class for name and edges: Triangle
 class triangle(shapeNames): 
 	def getEdges(self):  
 		return 3  
@@ -36,6 +43,7 @@ class triangle(shapeNames):
 	def displayFunction(self): 
 		print("triangle")
 
+# This is our database, where each item is a shape. 
 shapeList = [square(), circle(), triangle()] 
 
 # Sorts based on the number of edges of the shape
