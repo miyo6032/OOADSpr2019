dc21f2b7a0c3ffff7cba9c2e1e7f50f53a4d2b52
miyo6032 <miyo6032@colorado.edu>
2019-04-07 18:25:10 -0600

    Project operations for database

--- HomeworkFour/Database.py
+++ HomeworkFour/Database.py
@@ -1,25 +1,43 @@
 import pymongo as pm
 
+# Responsible for holding task data
 class Task:
-    def __init__(self, name, deadline, description, members):
-        self.__name = name
-        self.__deadline = deadline
-        self.__description = description
-        self.__team_members = members
+    def __init__(self, name = "", deadline = 0, description = "", members = [], task = None):
+        if task == None:
+            self.__name = name
+            self.__deadline = deadline
+            self.__description = description
+            self.__team_members = members
+        else:
+            self.__name = task["name"]
+            self.__deadline = task["deadline"]
+            self.__description = task["description"]
+            self.__team_members = task["members"]
+
+    def get_json(self):
+        return {"name": self.__name, "deadline": self.__deadline, "description": self.__description, "members": self.__members}
 
     def __str__(self):
-        return "Task " + self.__name
+        return self.get_json().__str__()
 
     def __repr__(self):
         return self.__str__()
 
+# Responsible for keeping track of its tasks
 class Project:
-    def __init__(self, name, deadline, description, members):
-        self.__tasks = []
-        self.__name = name
-        self.__deadline = deadline
-        self.__description = description
-        self.__team_members = members
+    def __init__(self, name = "", deadline = 0, description = "", members = [], project = None):
+        if project == None:
+            self.__tasks = []
+            self.__name = name
+            self.__deadline = deadline
+            self.__description = description
+            self.__members = members
+        else:
+            self.__tasks = project["tasks"]
+            self.__name = project["name"]
+            self.__deadline = project["deadline"]
+            self.__description = project["description"]
+            self.__members = project["members"]
 
     def add_task(self, task):
         self.__tasks.append(task)
@@ -27,8 +45,13 @@ class Project:
     def remove_task(self, task):
         self.__tasks.remove(task)
 
+    def get_json(self):
+        tasks = [task.get_json() for task in self.__tasks]
+        json = {"tasks" : tasks, "name": self.__name, "deadline": self.__deadline, "description": self.__description, "members": self.__members}
+        return json
+
     def __str__(self):
-        return "Project " + self.__name
+        return self.get_json().__str__()
 
     def __repr__(self):
         return self.__str__()
@@ -44,6 +67,7 @@ class Subject:
         for observer in self.__observers:
             observer.update()
 
+# Responsible for managing all of the project through the database
 class Database(Subject):
     __instance = None
 
@@ -53,7 +77,9 @@ class Database(Subject):
             raise Exception("Attempt to more than one Database singleton instance.")
         
         Database.__instance = self
-        self.__projects = []
+        db_server = pm.MongoClient("mongodb://localhost:27017/")
+        the_database = db_server["project_database"]
+        self.__saved_projects = the_database["projects"]
 
     @staticmethod
     def get_instance(): # Can be accessed anywhere to get the instance of database
@@ -62,24 +88,29 @@ class Database(Subject):
         return Database.__instance
 
     def get_projects(self):
-        return self.__projects
+        return [Project(project = json) for json in self.__saved_projects.find()]
 
     def delete_project(self, project):
-        self.__projects.remove(project)
+        self.__saved_projects.delete_one(project.get_json())
 
     def add_project(self, project):
-        self.__projects.append(project)
+        self.__saved_projects.insert_one(project.get_json())
 
-    def load(self):
-        pass
-
-    def save(self):
-        pass 
+    # Update a project by passing in the project and a dictionary key value for which values to update
+    def update_project(self, project, update):
+        self.__saved_projects.update_one(project.get_json(), { "$set": update })
 
 if __name__ == "__main__":
-    db_server = pm.MongoClient("mongodb://localhost:27017/")
-    the_database = db_server["project_database"]
-    projects = the_database["projects"]
-    project = {"name" : "yodo", "deadline" : 4, "description" : "bashi", "members" : ["Power", "Sasha"]}
-    for project in projects.find():
-        print(project)
+    d = Database.get_instance();
+    p = Project(name = "Yodo", deadline = 4, description = "bashi", members = ["barribob", "power"])
+    d.add_project(p);
+    # d.delete_project(p);
+
+    for p in d.get_projects():
+        print(p)
+
+    for p in d.get_projects():
+        d.update_project(p, {"name": "Yolo"})
+
+    for p in d.get_projects():
+        print(p)
